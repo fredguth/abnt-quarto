ca6b8b7bb552764c11d8ff29583c138e416fe08b
Fred Guth <fredguth@fredguth.com>
2023-01-16 22:53:04 -0300

    tese da Anna como exemplo

--- _extensions/abnt/rename.py
+++ _extensions/abnt/rename.py
@@ -1,7 +1,9 @@
 
 import os
 from os.path import exists
-file_exists = exists(os.getcwd()+'/docs/{{--meta-abnt.tipo_documento--}}.pdf')
-if file_exists:
-    os.rename('docs/{{--meta-abnt.tipo_documento--}}.pdf', 'docs/monografia.pdf')
-    print("rename.py:: Output created: docs/monografia.pdf")
+
+path = os.getcwd()+"/docs/"
+for filename in os.listdir(path):
+    if filename[-3:] == "pdf":
+        os.rename(path+"/"+filename, path+"/monografia.pdf")
+        print("rename.py:: Output created: docs/monografia.pdf")
